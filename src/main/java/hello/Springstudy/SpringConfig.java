e6d327b9ee38861b62cc4fcb8f9a05943cf0e886
jd053303 <jd053303@gmail.com>
2021-08-28 20:07:51 +0900

    feat: JdbcTemplate

--- src/main/java/hello/Springstudy/SpringConfig.java
+++ src/main/java/hello/Springstudy/SpringConfig.java
@@ -1,31 +1,27 @@
 package hello.Springstudy;
 
 import hello.Springstudy.repository.JdbcMemberRepository;
+import hello.Springstudy.repository.JdbcTemplateMemberRepository;
 import hello.Springstudy.repository.MemberRepository;
 import hello.Springstudy.repository.MemoryMemberRepository;
 import hello.Springstudy.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
-    @Autowired
+    private final DataSource dataSource;
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+// return new MemoryMemberRepository();
+// return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
